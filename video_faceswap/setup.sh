3472cf4d9b6b07070cbcb038072d30f111426128
FuChen <fuchen@foxmail.com>
2024-01-24 15:14:26 +0800

    add setup.sh

--- video_faceswap/setup.sh
+++ video_faceswap/setup.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+git clone https://github.com/yytdfc/aws_notebooks.git
+cd aws_notebooks/video_faceswap
 conda env create -f conda_env.yaml
-conda activate insightface
+# conda activate insightface
