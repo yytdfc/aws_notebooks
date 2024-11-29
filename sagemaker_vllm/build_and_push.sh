04bc64c805c78c091a6c13a0b15f1b98b8304f13
FuChen <fuchen@foxmail.com>
2024-11-29 10:16:04 +0800

    update sagemaker vllm endpoint for China region

--- sagemaker_vllm/build_and_push.sh
+++ sagemaker_vllm/build_and_push.sh
@@ -8,9 +8,6 @@ REPO_NAMESPACE=${REPO_NAMESPACE:-"sagemaker_endpoint/vllm"}
 ACCOUNT=${ACCOUNT:-$(aws sts get-caller-identity --query Account --output text)}
 REGION=${REGION:-$(aws configure get region)}
 
-REPO_NAME="${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAMESPACE}:${VLLM_VERSION}"
-echo ${REPO_NAME}
-
 # If the repository doesn't exist in ECR, create it.
 aws ecr describe-repositories --repository-names "${REPO_NAMESPACE}" > /dev/null 2>&1
 if [ $? -ne 0 ]
@@ -20,7 +17,15 @@ aws ecr create-repository --repository-name "${REPO_NAMESPACE}" > /dev/null
 fi
 
 # Log into Docker
-aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com
+if [[ "$REGION" = cn* ]]; then
+    aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com.cn
+    REPO_NAME="${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com.cn/${REPO_NAMESPACE}:${VLLM_VERSION}"
+else
+    aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com
+    REPO_NAME="${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAMESPACE}:${VLLM_VERSION}"
+fi
+
+echo ${REPO_NAME}
 
 # Build docker
 docker build --build-arg VLLM_VERSION=${VLLM_VERSION} --build-arg VLLM_REPO=${VLLM_REPO} -t ${REPO_NAMESPACE}:${VLLM_VERSION} .
